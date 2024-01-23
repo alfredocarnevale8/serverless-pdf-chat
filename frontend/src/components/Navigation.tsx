95af237a643b0c093a233a133ca5a0494d910435
alfredocarnevale8 <38814136+alfredocarnevale8@users.noreply.github.com>
2024-01-23 15:06:21 +0100

    Update Navigation.tsx

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -26,7 +26,8 @@ const Navigation: React.FC<NavigationProps> = ({
         >
           <img 
             src = {Reply}
-            
+            height = {20}
+            width = {20}
             className = "w-6 h-6 mr-1.5"
             />
           <ChatBubbleLeftRightIcon className="w-6 h-6 mr-1.5" />
