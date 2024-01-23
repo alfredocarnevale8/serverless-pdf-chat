6aec63eaa26ea2dac769e983ef770cd5993bb4ab
alfredocarnevale8 <38814136+alfredocarnevale8@users.noreply.github.com>
2024-01-23 16:03:21 +0100

    Update Navigation.tsx

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -20,12 +20,6 @@ const Navigation: React.FC<NavigationProps> = ({
   return (
     <nav className="bg-green-600">
       <div className="container flex flex-wrap items-center justify-between py-3">
-      <img 
-                src = {Reply}
-                className = "w-10 h-10 mr-1.5 font bold"
-                />
-        </div>
-      <div className="container flex flex-wrap items-center justify-between py-3">
         <Link
           to="/"
           className="inline-flex items-center self-center text-2xl font-semibold whitespace-nowrap text-white"
