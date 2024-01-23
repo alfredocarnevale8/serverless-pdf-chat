64de203b5412f0a2ec097b25f25eaf13e3730206
alfredocarnevale8 <38814136+alfredocarnevale8@users.noreply.github.com>
2024-01-23 12:29:19 +0100

    Update Navigation.tsx

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -18,7 +18,7 @@ const Navigation: React.FC<NavigationProps> = ({
   handleSignOutClick,
 }: NavigationProps) => {
   return (
-    <nav className="bg-green-700">
+    <nav className="bg-green-600">
       <div className="container flex flex-wrap items-center justify-between py-3">
         <Link
           to="/"
